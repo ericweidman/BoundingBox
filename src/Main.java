d49a949c9c3e3131588303d20fff759ffe3ea76d
ericweidman <ericweidman@gmail.com>
2016-06-28 18:18:30 -0400

    Added saveFile method.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         double userUpperY = 180;
-        double userLowerY = 0;
-        double userLeftX = 0;
-        double userRightX= 360;
+        double userLowerY = 179;
+        double userLeftX = 359;
+        double userRightX = 360;
+        String writeThis = "";
+
 
         ArrayList<Location> outputLocations = new ArrayList<>();
         ArrayList<Location> allLocations = new ArrayList<>();
@@ -18,22 +22,35 @@ public class Main {
 
 
         for (Location location : allLocations) {
-            if((location.getxValue() >= userLeftX && location.getxValue() <= userRightX) && (location.getyValue() >= userLowerY && location.getyValue() <= userUpperY )){
-             outputLocations.add(location);
+            if ((location.getxValue() >= userLeftX && location.getxValue() <= userRightX) && (location.getyValue() >= userLowerY && location.getyValue() <= userUpperY)) {
+                outputLocations.add(location);
+
             }
         }
-        System.out.println(outputLocations.size());
 
+        for (Location location : outputLocations) {
+            writeThis += Double.toString(location.getxValue()) + "," + Double.toString(location.getyValue()) + "," + Double.toString(location.getData()) + "\n";
+
+        }
+        saveFile("query_results.txt", writeThis);
     }
+
 
     static void openFile(ArrayList<Location> allLocations) throws FileNotFoundException {
         File f = new File("sample_data.csv");
         Scanner fileScanner = new Scanner(f);
-        while(fileScanner.hasNext()){
+        while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
             String[] columns = line.split(",");
             Location location = new Location(Double.parseDouble(columns[0]), Double.parseDouble(columns[1]), Double.parseDouble(columns[2]));
             allLocations.add(location);
         }
+    }
+
+    static void saveFile(String fileName, String fileContent) throws IOException {
+        File f = new File(fileName);
+        FileWriter fw = new FileWriter(f);
+        fw.write(fileContent);
+        fw.close();
     }
 }
